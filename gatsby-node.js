a661dd9dc7b10dc4b767496f2a5959d5b0e28376
peterlazzarino <peter.lazzarino@gmail.com>
2018-11-23 10:40:17 -0500

    works for me on windows

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,18 @@
-const { fmImagesToRelative } = require('gatsby-remark-relative-images');
+const path = require("path");
+const deepMap = require("deep-map");
 
 exports.onCreateNode = ({ node }) => {
-  fmImagesToRelative(node);
+	if (node.internal.type === "MarkdownRemark") {
+		deepMap(node.frontmatter, (value) => {
+			let newPath = value;
+			if (typeof value === "string" && path.isAbsolute(value) && value.indexOf("/assets/") !== -1) {
+        const mdPath = node.fileAbsolutePath;
+        newPath = path.join(`../../`, "static", value);
+        console.log(newPath)
+			}
+			return newPath;
+		}, {
+			inPlace: true
+		});
+	}
 };
